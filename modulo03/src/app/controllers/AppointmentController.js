482efe5a83aa5ff6ce5c9034eaa948ea71e5be8d
Binho <jose.rubens.veiga@gmail.com>
2020-02-07 19:21:45 -0300

    Add module 03

---
+++ modulo03/src/app/controllers/AppointmentController.js
@@ -0,0 +1,178 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
+import Notification from '../schemas/Notification';
+
+import User from '../models/User';
+import File from '../models/File';
+import Appointment from '../models/Appointment';
+
+import CancellationMail from "../jobs/CancellationMail";
+
+import Queue from '../../lib/Queue';
+
+class AppointmentController {
+  async index(req, res) {
+
+    const { page = 1 } = req.query;
+    const perPage = 20;
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: 2,
+        canceled_at: null
+      },
+      order: ['date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
+      limit: perPage,
+      offset: (page - 1) * perPage,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name', 'email'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['name', 'url', 'path'],
+            },
+          ],
+        },
+      ],
+    })
+    
+    return res.json(appointments);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      date: Yup.date().required(),
+      provider_id: Yup.number().required(),
+    });
+
+    if(! (await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'Validation fails',
+      });
+    }
+
+    
+    const { provider_id, date } = req.body;
+    const isSameUser = provider_id == req.userId;
+
+    if(isSameUser) {
+      return res.status(400).json({
+        error: 'You can only create appointments with other users',
+      });
+    }
+    
+    const isProvider = await User.findOne({
+      where: {
+        id: provider_id,
+        provider: true,
+      },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({
+        error: 'You can only create appointments with providers',
+      });
+    }
+
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        error: 'Past dates are not permitted',
+      });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not available'});
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    const { name: userName } = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      'dd \'de\' MMMM\', às\' H:mm\'h\'',
+      { locale: pt },
+    );
+
+    await Notification.create({
+      content: `Novo Agendamento de ${userName} para o dia ${formattedDate}`,
+      user: provider_id
+    });
+    return res.json(appointment);
+  }
+
+  async destroy(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name']
+        }
+      ],
+    });
+    const now = new Date();
+
+    if (!appointment) {
+      return res.status(400).json({
+        error: 'There is no appointment with this id',
+      });
+    }
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: 'You don\'t have permission to cancel this appointment'
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, now)) {
+      return res.status(401).json({
+        error: 'Your can only cancel appointments 2 hours in advance',
+      });
+    }
+
+    if(appointment.canceled_at !== null) {
+      return res.status(400).json({
+        error: 'This appointment has already been canceled'
+      });
+    }
+
+    appointment.canceled_at = now;
+    await appointment.save();
+
+    await Queue.add(CancellationMail.key, {
+      appointment
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
